6c8492d9afa8323c36891b254cb24538f00e79e8
Kevin Durbin <kevindurb@gmail.com>
2022-12-28 16:09:09 -0700

    update install.sh, pull latest antigen on install

--- install.sh
+++ install.sh
@@ -1,24 +1,41 @@
 #! /bin/bash
 
-echo "Check for dependencies"
+echo "-- Check for dependencies"
 if ! command -v git || ! command -v rcup || ! command -v zsh; then
   echo "Missing dependencies!"
   exit 1;
 fi
 
-echo "Store current dir and move to home"
+echo
+echo "-- Store current dir and move to home"
 pushd . &> /dev/null
-cd ~
+cd ~ || exit
 
-echo "Clone dotfiles repo"
-git clone git@github.com:kevindurb/.dotfiles.git
-cd ~/.dotfiles
+echo
+echo "-- Install antigen"
+curl -L git.io/antigen > ~/antigen.zsh 2> /dev/null
 
-echo "Do first rcup to get .rcrc setup correctly"
+echo
+if [[ -d ~/.dotfiles ]]
+then
+  echo "-- Dotfiles already cloned"
+else
+  echo "-- Clone dotfiles repo"
+  git clone git@github.com:kevindurb/.dotfiles.git
+fi
+
+echo
+echo "-- Move into ~/.dotfiles"
+cd ~/.dotfiles || exit
+
+echo
+echo "-- Do first rcup to get .rcrc setup correctly"
 RCRC=~/.dotfiles/rcrc rcup
 
-echo "Setup zsh as default shell"
-chsh -s $(which zsh)
+echo
+echo "-- Setup zsh as default shell"
+chsh -s "$(which zsh)"
 
-echo "Return to where ever we were at the beginning"
-popd &> /dev/null
+echo
+echo "-- Return to where ever we were at the beginning"
+popd &> /dev/null || exit
