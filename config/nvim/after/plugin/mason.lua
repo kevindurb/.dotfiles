09983b4e37beabce5916055a3a47eb99e80b3638
Kevin Durbin <kevindurb@fastmail.com>
2023-05-08 21:51:07 -0600

    ignore vim global

--- config/nvim/after/plugin/mason.lua
+++ config/nvim/after/plugin/mason.lua
@@ -1,7 +1,15 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 
-require("lspconfig").lua_ls.setup {}
+require("lspconfig").lua_ls.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
+}
 require("lspconfig").tsserver.setup {}
 require("lspconfig").ansiblels.setup {}
 require("lspconfig").bashls.setup {}
