d549f855066a04b819965fb6f0d098ce991292bd
Kevin Durbin <kevindurb@fastmail.com>
2023-05-15 10:20:45 -0600

    add support for hyper terminal

---
+++ hyper.js
@@ -0,0 +1,17 @@
+"use strict";
+module.exports = {
+  config: {
+    catppuccinTheme: 'Macchiato',
+    updateChannel: 'stable',
+    fontSize: 12,
+    fontFamily: '"FiraCode Nerd Font", monospace',
+    fontWeight: 'normal',
+    fontWeightBold: 'bold',
+    webGLRenderer: false,
+    padding: '0 0',
+  },
+  plugins: [
+    'hypurr#latest',
+    'hyper-font-ligatures'
+  ],
+};
