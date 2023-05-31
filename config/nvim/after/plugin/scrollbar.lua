4772988409ecfc2e7c181314a49bbaec2ab13297
Kevin Durbin <kevindurb@fastmail.com>
2023-05-31 14:51:05 -0600

    subtle scrollbar

--- config/nvim/after/plugin/scrollbar.lua
+++ config/nvim/after/plugin/scrollbar.lua
@@ -1,3 +1,15 @@
-require('scrollbar').setup()
+local colors = require("catppuccin.palettes").get_palette "macchiato"
+
+require('scrollbar').setup({
+  handle = {text = ' ', color = colors.surface2},
+  handlers = {
+    cursor = false,
+    diagnostic = true,
+    gitsigns = true,
+    handle = true,
+    search = true,
+    ale = false
+  }
+})
 require('scrollbar.handlers.search').setup()
 require('scrollbar.handlers.gitsigns').setup()
