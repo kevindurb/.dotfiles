14ef2cba61a4216cbe8632af7c1759618e3484aa
Kevin Durbin <kevindurb@fastmail.com>
2023-09-08 15:29:48 -0600

    chore: clean up init.vim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,52 +2,46 @@ require('kevindurb')
 local packer_bootstrap = require('packer-install').ensure_packer()
 
 require('packer').startup(function(use)
-  use('wbthomason/packer.nvim')
-  use({ 'catppuccin/nvim', as = 'catppuccin' })
-  use('tpope/vim-sensible') -- a good place to start
-  use('tpope/vim-vinegar') -- best navigation with -
-  use('tpope/vim-fugitive') -- git commands in vim
-  use('tpope/vim-repeat') -- repeating plugin commands
-  use('tpope/vim-surround') -- surround with things
-  use('tpope/vim-unimpaired') -- mappings for things with '['
-  use('tpope/vim-eunuch') -- helpful unixy commands
-  use('tpope/vim-abolish') -- messing with words and casing
-  use('alvan/vim-closetag') -- autoclose tags
-  use('Raimondi/delimitMate') -- auto close everything else
-  use('christoomey/vim-tmux-navigator') -- navigate panes in tmux && vim
-  use('godlygeek/tabular') -- auto alignment
-  use('NvChad/nvim-colorizer.lua') -- colorize css colors
-  use('onsails/lspkind.nvim') -- Vscode style icons
-  use('L3MON4D3/LuaSnip') -- snippet engine
-  use('NoahTheDuke/vim-just') -- justfile
   use({
-    'rcarriga/nvim-dap-ui',
-    'mfussenegger/nvim-dap',
-    'jay-babu/mason-nvim-dap.nvim',
+    'wbthomason/packer.nvim',
+    { 'catppuccin/nvim', as = 'catppuccin' },
+    'tpope/vim-sensible', -- a good place to start
+    'tpope/vim-vinegar', -- best navigation with -
+    'tpope/vim-fugitive', -- git commands in vim
+    'tpope/vim-repeat', -- repeating plugin commands
+    'tpope/vim-surround', -- surround with things
+    'tpope/vim-unimpaired', -- mappings for things with '['
+    'tpope/vim-eunuch', -- helpful unixy commands
+    'tpope/vim-abolish', -- messing with words and casing
+    'alvan/vim-closetag', -- autoclose tags
+    'Raimondi/delimitMate', -- auto close everything else
+    'christoomey/vim-tmux-navigator', -- navigate panes in tmux && vim
+    'godlygeek/tabular', -- auto alignment
+    'NvChad/nvim-colorizer.lua', -- colorize css colors
+    'onsails/lspkind.nvim', -- Vscode style icons
+    'L3MON4D3/LuaSnip', -- snippet engine
+    'NoahTheDuke/vim-just', -- justfile
+    'rcarriga/nvim-dap-ui', -- debugging ui
+    'mfussenegger/nvim-dap', -- debugging
+    'Glench/Vim-Jinja2-Syntax', -- jinja2 syntax
+    'lewis6991/gitsigns.nvim', -- pretty gitsigns in gutter
+    'petertriho/nvim-scrollbar', -- a scrollbar
+    {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+    }, -- pretty lower bar
+    {
+      'akinsho/bufferline.nvim',
+      tag = '*',
+      requires = 'nvim-tree/nvim-web-devicons',
+    }, -- pretty tabs
+    {
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.x',
+      requires = { { 'nvim-lua/plenary.nvim' } },
+    }, -- pretty search
+    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }, -- use native fzf
   })
-  use('Glench/Vim-Jinja2-Syntax')
-  use('lewis6991/gitsigns.nvim')
-  use('kevinhwang91/nvim-hlslens')
-  use('folke/trouble.nvim')
-  use('petertriho/nvim-scrollbar')
-  use({
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
-  })
-  use({
-    'akinsho/bufferline.nvim',
-    tag = '*',
-    requires = 'nvim-tree/nvim-web-devicons',
-  })
-
-  use({
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.x',
-    requires = { { 'nvim-lua/plenary.nvim' } },
-  })
-
-  use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
-
   use({
     'nvim-treesitter/nvim-treesitter',
     run = function()
