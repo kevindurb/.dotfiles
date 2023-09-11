91ad33eab9bf6124202f435226b222ea5f5c109d
Kevin Durbin <kevindurb@fastmail.com>
2023-09-11 08:07:29 -0600

    chore: switch to lazy nvim

--- config/nvim/after/plugin/zen-mode.lua
+++
@@ -1,8 +0,0 @@
-require('zen-mode').setup({
-  plugins = {
-    twilight = { enabled = true },
-    gitsigns = { enabled = true },
-    tmux = { enabled = false },
-    kitty = { enabled = true, font = '+4' },
-  },
-})
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,71 +1,59 @@
 require('kevindurb')
-local packer_bootstrap = require('packer-install').ensure_packer()
-
-require('packer').startup(function(use)
-  use({
-    'wbthomason/packer.nvim',
-    { 'catppuccin/nvim', as = 'catppuccin' },
-    'tpope/vim-sensible', -- a good place to start
-    'tpope/vim-vinegar', -- best navigation with -
-    'tpope/vim-fugitive', -- git commands in vim
-    'tpope/vim-repeat', -- repeating plugin commands
-    'tpope/vim-surround', -- surround with things
-    'tpope/vim-unimpaired', -- mappings for things with '['
-    'tpope/vim-eunuch', -- helpful unixy commands
-    'tpope/vim-abolish', -- messing with words and casing
-    'alvan/vim-closetag', -- autoclose tags
-    'Raimondi/delimitMate', -- auto close everything else
-    'christoomey/vim-tmux-navigator', -- navigate panes in tmux && vim
-    'godlygeek/tabular', -- auto alignment
-    'NvChad/nvim-colorizer.lua', -- colorize css colors
-    'onsails/lspkind.nvim', -- Vscode style icons
-    'L3MON4D3/LuaSnip', -- snippet engine
-    'NoahTheDuke/vim-just', -- justfile
-    'rcarriga/nvim-dap-ui', -- debugging ui
-    'mfussenegger/nvim-dap', -- debugging
-    'Glench/Vim-Jinja2-Syntax', -- jinja2 syntax
-    'lewis6991/gitsigns.nvim', -- pretty gitsigns in gutter
-    'petertriho/nvim-scrollbar', -- a scrollbar
-    'nvim-lualine/lualine.nvim', -- pretty lower bar
-    'akinsho/bufferline.nvim', -- pretty tabs
-    {
-      'nvim-telescope/telescope.nvim',
-      tag = '0.1.x',
-      requires = {
-        'nvim-lua/plenary.nvim',
-        {
-          'nvim-telescope/telescope-fzf-native.nvim',
-          run = 'make',
-        },
+require('lazy-install').ensure_lazy()
+require('lazy').setup({
+  'wbthomason/packer.nvim',
+  { 'catppuccin/nvim', name = 'catppuccin' },
+  'tpope/vim-sensible', -- a good place to start
+  'tpope/vim-vinegar', -- best navigation with -
+  'tpope/vim-fugitive', -- git commands in vim
+  'tpope/vim-repeat', -- repeating plugin commands
+  'tpope/vim-surround', -- surround with things
+  'tpope/vim-unimpaired', -- mappings for things with '['
+  'tpope/vim-eunuch', -- helpful unixy commands
+  'tpope/vim-abolish', -- messing with words and casing
+  'alvan/vim-closetag', -- autoclose tags
+  'Raimondi/delimitMate', -- auto close everything else
+  'christoomey/vim-tmux-navigator', -- navigate panes in tmux && vim
+  'godlygeek/tabular', -- auto alignment
+  'NvChad/nvim-colorizer.lua', -- colorize css colors
+  'onsails/lspkind.nvim', -- Vscode style icons
+  'L3MON4D3/LuaSnip', -- snippet engine
+  'NoahTheDuke/vim-just', -- justfile
+  'rcarriga/nvim-dap-ui', -- debugging ui
+  'mfussenegger/nvim-dap', -- debugging
+  'Glench/Vim-Jinja2-Syntax', -- jinja2 syntax
+  'lewis6991/gitsigns.nvim', -- pretty gitsigns in gutter
+  'petertriho/nvim-scrollbar', -- a scrollbar
+  'nvim-lualine/lualine.nvim', -- pretty lower bar
+  'akinsho/bufferline.nvim', -- pretty tabs
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.2',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
       },
-    }, -- pretty search
-    {
-      'nvim-treesitter/nvim-treesitter',
-      run = function()
-        local ts_update = require('nvim-treesitter.install').update({
-          with_sync = true,
-        })
-        ts_update()
-      end,
-    }, -- the base for all things better
-    'williamboman/mason.nvim', -- manages external editor tools
-    'williamboman/mason-lspconfig.nvim', -- connects mason to lspconfig
-    'neovim/nvim-lspconfig', -- language server configurations
-    'hrsh7th/nvim-cmp', -- auto complete menu
-    'hrsh7th/cmp-nvim-lsp', -- lsp in auto complete menu
-    'hrsh7th/cmp-buffer', -- auto complete buffer text
-    'f3fora/cmp-spell', -- auto complete spelling
-    'petertriho/cmp-git', -- auto complete git things
-    'davidsierradz/cmp-conventionalcommits', -- auto complete conventional commits
-    'mhartington/formatter.nvim', -- auto format files
-    'nvim-tree/nvim-web-devicons', -- cool icons
-    'b0o/schemastore.nvim', -- json schemas
-    { 'utilyre/barbecue.nvim', requires = { 'SmiteshP/nvim-navic' } }, -- breadcrumb bar
-    'mfussenegger/nvim-lint', -- linting based on lsp
-    'numToStr/Comment.nvim', -- commenting based on lsp
-  })
-
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+    },
+  }, -- pretty search
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+  }, -- the base for all things better
+  'williamboman/mason.nvim', -- manages external editor tools
+  'williamboman/mason-lspconfig.nvim', -- connects mason to lspconfig
+  'neovim/nvim-lspconfig', -- language server configurations
+  'hrsh7th/nvim-cmp', -- auto complete menu
+  'hrsh7th/cmp-nvim-lsp', -- lsp in auto complete menu
+  'hrsh7th/cmp-buffer', -- auto complete buffer text
+  'f3fora/cmp-spell', -- auto complete spelling
+  'petertriho/cmp-git', -- auto complete git things
+  'davidsierradz/cmp-conventionalcommits', -- auto complete conventional commits
+  'mhartington/formatter.nvim', -- auto format files
+  'nvim-tree/nvim-web-devicons', -- cool icons
+  'b0o/schemastore.nvim', -- json schemas
+  { 'utilyre/barbecue.nvim', dependencies = { 'SmiteshP/nvim-navic' } }, -- breadcrumb bar
+  'mfussenegger/nvim-lint', -- linting based on lsp
+  'numToStr/Comment.nvim', -- commenting based on lsp
+})
