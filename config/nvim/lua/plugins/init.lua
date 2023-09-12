8313e113389adc46875e97e8c36f946f4348eddc
Kevin Durbin <kevindurb@fastmail.com>
2023-09-12 13:31:47 -0600

    chore: try out different tmux nvim integration plugin

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,14 +1,12 @@
 -- Just basic plugins that dont need
 -- any setup
 return {
-  -- 'tpope/vim-vinegar', -- best navigation with -
   'tpope/vim-fugitive', -- git commands in vim
   'tpope/vim-repeat', -- repeating plugin commands
   'tpope/vim-surround', -- surround with things
   'tpope/vim-unimpaired', -- mappings for things with '['
   'tpope/vim-eunuch', -- helpful unixy commands
   'tpope/vim-abolish', -- messing with words and casing
-  'christoomey/vim-tmux-navigator', -- navigate panes in tmux && vim
   'godlygeek/tabular', -- auto alignment
   { 'NoahTheDuke/vim-just', ft = 'just' }, -- justfile
   { 'Glench/Vim-Jinja2-Syntax', ft = 'jinja2' }, -- jinja2 syntax
