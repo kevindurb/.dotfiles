0b0bf8800288753e0963a0bbd4d2c4cc05ae4294
Kevin Durbin <kevindurb@fastmail.com>
2023-11-27 08:29:06 -0700

    feat: testing some new plugins

--- config/nvim/lua/plugins/closetag.lua
+++ config/nvim/lua/plugins/closetag.lua
@@ -1,5 +1,6 @@
 return {
   'alvan/vim-closetag', -- autoclose tags
+  enable = false,
   init = function()
     vim.g.closetag_filenames = '*.html,*.xhtml,*.phtml,*.erb,*.jsx,*.tsx,*.js'
   end,
--- config/nvim/lua/plugins/delimitmate.lua
+++ config/nvim/lua/plugins/delimitmate.lua
@@ -1,5 +1,6 @@
 return {
   'Raimondi/delimitMate', -- auto close everything else
+  enable = false,
   init = function()
     vim.g.delimitMate_matchpairs = '(:),[:],{:}'
   end,
