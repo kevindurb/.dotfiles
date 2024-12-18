b2fb441a22a0135f4a41c0f3276f83f10f5b1076
Kevin Durbin <kevindurb@fastmail.com>
2024-12-18 16:35:45 -0700

    move packages to nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -9,10 +9,63 @@ in {
 
     stateVersion = "24.11"; # Please read the comment before changing.
     packages = with pkgs; [
+      aws-vault
+      awscli
+      bat
+      cmatrix
       cowsay
+      curl
+      duf
+      difftastic
+      entr
+      eza
+      fastfetch
+      figlet
+      fzf
+      gd
+      gh
+      git
+      delta
+      git-extras
+      gitmux
+      go-task
+      htop
+      httpie
+      jq
+      kubectl
+      krew
+      kustomize
+      lazygit
+      litecli
+      lolcat
+      mariadb
+      mycli
+      ncdu
+      neovim
+      noti
+      nyancat
+      ollama
+      openssh
+      pgcli
+      prettyping
+      pv
+      ripgrep
+      starship
+      tig
+      tldr
+      tmux
+      tree
+      yq
+      zoxide
     ];
     file = {};
     sessionVariables = {};
+  };
+
+  services = {
+      syncthing = {
+          enable = true;
+      };
   };
 
   programs.home-manager.enable = true;
