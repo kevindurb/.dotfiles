2cb48e1a7b47eeeb802602c2bf3af2b87ea89766
Kevin Durbin <kevindurb@fastmail.com>
2024-12-19 11:26:53 -0700

    include home.nix

---
+++ .config/nix/common/home.nix
@@ -0,0 +1,66 @@
+{ pkgs, ... }:
+
+{
+  home = {
+    stateVersion = "24.11";
+    packages = with pkgs; [
+      aws-vault
+      awscli
+      bat
+      cmatrix
+      cowsay
+      curl
+      duf
+      difftastic
+      entr
+      eza
+      fastfetch
+      figlet
+      fzf
+      gd
+      gh
+      git
+      delta
+      git-extras
+      gitmux
+      go-task
+      htop
+      httpie
+      jq
+      kubectl
+      krew
+      kustomize
+      lazygit
+      litecli
+      lolcat
+      mariadb
+      mycli
+      ncdu
+      neovim
+      noti
+      nyancat
+      ollama
+      openssh
+      pgcli
+      prettyping
+      pv
+      ripgrep
+      starship
+      tig
+      tldr
+      tmux
+      tree
+      yq
+      zoxide
+    ];
+    file = {};
+    sessionVariables = {};
+  };
+
+  services = {
+      syncthing.enable = true;
+      # ollama.enable = true;
+  };
+
+  programs.home-manager.enable = true;
+}
