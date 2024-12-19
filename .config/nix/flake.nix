d7da7fa4771e8acf14bd43635b3eaec4e5cb92cc
Kevin Durbin <kevindurb@fastmail.com>
2024-12-19 10:54:10 -0700

    remove brew, finalize nix

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -20,7 +20,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.kevindurbin = import ./workmac/home.nix;
+            home-manager.users.kevindurbin = import ./common/home.nix;
           }
         ];
       };
