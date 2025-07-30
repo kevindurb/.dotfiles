0afb6a352449f8094223c943c6c92a834cca1447
Kevin Durbin <kevindurb@fastmail.com>
2025-07-30 16:03:00 -0600

    clean up a bunch of unused stuff

--- .config/nvim/lua/plugins/nord.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'shaunsingh/nord.nvim',
-  enabled = false,
-  init = function()
-    vim.g.nord_disable_background = true
-    vim.o.background = 'dark'
-    vim.cmd.colorscheme('nord')
-  end,
-}
