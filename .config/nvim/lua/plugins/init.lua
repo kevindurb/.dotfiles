27a2886598cf168adb0dc822a40a60c745ab74e9
Kevin Durbin <kevindurb@fastmail.com>
2025-08-11 12:13:20 -0600

    try to fix ibl

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,11 +1,11 @@
 -- Just basic plugins that dont need
 -- any setup
 return {
-  'tpope/vim-fugitive',                                 -- git commands in vim
-  'tpope/vim-repeat',                                   -- repeating plugin commands
-  'tpope/vim-unimpaired',                               -- mappings for things with '['
-  'tpope/vim-eunuch',                                   -- helpful unixy commands
-  'tpope/vim-abolish',                                  -- messing with words and casing
-  'godlygeek/tabular',                                  -- auto alignment
-  { 'lukas-reineke/indent-blankline.nvim', opts = {} }, -- indent guides
+  'tpope/vim-fugitive',                                               -- git commands in vim
+  'tpope/vim-repeat',                                                 -- repeating plugin commands
+  'tpope/vim-unimpaired',                                             -- mappings for things with '['
+  'tpope/vim-eunuch',                                                 -- helpful unixy commands
+  'tpope/vim-abolish',                                                -- messing with words and casing
+  'godlygeek/tabular',                                                -- auto alignment
+  { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} }, -- indent guides
 }
