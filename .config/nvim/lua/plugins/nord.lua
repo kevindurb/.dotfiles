07ababfe92fa709d43a07b914b54cbe3491290d5
Kevin Durbin <kevindurb@fastmail.com>
2025-08-21 12:30:10 -0600

    better nord theme

--- .config/nvim/lua/plugins/nord.lua
+++ .config/nvim/lua/plugins/nord.lua
@@ -1,13 +1,9 @@
 return {
-  'nordtheme/vim',
+  'shaunsingh/nord.nvim',
   name = 'nord',
   priority = 1000,
   enabled = true,
   init = function()
-    vim.g.nord_bold = true
-    vim.g.nord_underline = true
-    vim.g.nord_italic = true
-    vim.g.nord_italic_comments = true
     vim.cmd.colorscheme('nord')
   end,
 }
