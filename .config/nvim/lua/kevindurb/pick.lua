fea7542a0a2f7323bb782fa22fbe9231523a7f30
Kevin Durbin <kevindurb@fastmail.com>
2025-08-29 07:09:43 -0600

    add diagnostics picker

--- .config/nvim/lua/kevindurb/pick.lua
+++ .config/nvim/lua/kevindurb/pick.lua
@@ -4,7 +4,7 @@ M.setup = function()
   local later, add = require('mini.deps').later, require('mini.deps').add
   add({
     source = 'nvim-mini/mini.pick',
-    depends = { 'nvim-mini/mini.extra', 'nvim-mini/mini.icons' }
+    depends = { 'nvim-mini/mini.extra', 'nvim-mini/mini.icons' },
   })
 
   later(function()
@@ -13,6 +13,7 @@ M.setup = function()
     local pickers = require('mini.extra').pickers
     vim.keymap.set('n', '<C-p>', builtin.files, {})
     vim.keymap.set('n', '<C-o>', pickers.commands, {})
+    vim.keymap.set('n', '<leader>xx', pickers.diagnostics, {})
     vim.keymap.set('n', '<leader>b', builtin.buffers, {})
   end)
 end
