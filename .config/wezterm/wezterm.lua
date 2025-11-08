5920768137561d529b92aed9bb08e91ab0177bae
Kevin Durbin <kevindurb@fastmail.com>
2025-11-07 23:12:44 -0700

    better font rendering

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,5 +8,6 @@ config.default_prog = { 'zsh', '-l' }
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_wayland = true
 config.window_decorations = 'TITLE | RESIZE'
+config.freetype_load_target = 'Light'
 
 return config
