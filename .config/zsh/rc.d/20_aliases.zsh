3135c4d0df7d77c74bb0ea8cb6e6566ebe95c1d2
Kevin Durbin <kevindurb@fastmail.com>
2025-12-05 21:36:07 -0700

    remove unused things

--- .config/zsh/rc.d/20_aliases.zsh
+++ .config/zsh/rc.d/20_aliases.zsh
@@ -7,9 +7,8 @@
 alias dot="git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME"
 alias g=git
 alias c=clear
-alias gt="task --global"
 alias werk='GIT_SSH_COMMAND="ssh -i ~/.ssh/work -o IdentitiesOnly=yes" git'
 alias jme="jira issue list -q 'statusCategory != Done and assignee = currentUser()' --plain --order-by status"
 
-compdef dot=git g=git gt=task werk=git jme=jira
+compdef dot=git g=git werk=git
 setopt complete_aliases
