f3d8c04ee007567f724dda778ad0ac736cb31514
Kevin Durbin <kevindurb@fastmail.com>
2026-01-21 20:28:45 -0700

    use brew to install font

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
-config.font = wezterm.font('CaskaydiaMono Nerd Font')
+config.font = wezterm.font('Cascadia Code NF')
 config.font_size = 13
 config.color_scheme = 'Catppuccin Macchiato'
 config.hide_tab_bar_if_only_one_tab = true
