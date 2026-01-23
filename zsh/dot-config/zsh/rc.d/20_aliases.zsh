3dc6575af734a6bdd547520ece05d416ed206183
Kevin Durbin <kevindurb@fastmail.com>
2026-01-23 07:49:22 -0700

    **chore(git, zsh): disable default credential helpers and remove obsolete aliases**
    - Add empty `helper =` entries for `https://github.com` and `https://gist.github.com` to override any pre‑configured credential helper before invoking `gh auth git-credential`.
    - Remove the legacy `dot` alias that pointed to a separate dotfiles repository.
    - Remove the `werk` alias that forced a custom SSH key for work‑related Git operations.

--- zsh/dot-config/zsh/rc.d/20_aliases.zsh
+++ zsh/dot-config/zsh/rc.d/20_aliases.zsh
@@ -4,10 +4,8 @@
 [ -x "$(command -v prettyping)" ] && alias ping=prettyping
 [ -x "$(command -v eza)" ]        && alias ll="eza -lha --hyperlink --icons"
 
-alias dot="git --git-dir=$HOME/.dotfiles.git --work-tree=$HOME"
 alias g=git
 alias c=clear
-alias werk='GIT_SSH_COMMAND="ssh -i ~/.ssh/work -o IdentitiesOnly=yes" git'
 alias jme="jira issue list -q 'statusCategory != Done and assignee = currentUser()' --plain --order-by status"
 
 compdef dot=git g=git werk=git
