b515507485c04e3a9c64bbdfe8d00ea73aa30b8c
Kevin Durbin <kevindurb@fastmail.com>
2026-02-04 13:10:38 -0700

    solid color background, https git protocol

--- wezterm/dot-wezterm.lua
+++ wezterm/dot-wezterm.lua
@@ -8,7 +8,7 @@ config.hide_tab_bar_if_only_one_tab = true
 config.enable_wayland = true
 config.window_decorations = "RESIZE"
 config.freetype_load_target = "Light"
-config.window_background_opacity = 0.9
+config.window_background_opacity = 1
 
 config.keys = {
 	{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
