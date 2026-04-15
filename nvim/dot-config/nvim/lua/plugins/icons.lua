53ca12e0c87307c727a8c8820a6e01e85cc9b399
Kevin Durbin <kevindurb@fastmail.com>
2026-04-15 15:14:00 -0600

    clean up

--- nvim/dot-config/nvim/lua/plugins/icons.lua
+++ nvim/dot-config/nvim/lua/plugins/icons.lua
@@ -1,12 +1,4 @@
-require('mini.icons').setup({
-  file = {
-    ['.keep'] = { glyph = '󰊢', hl = 'MiniIconsGrey' },
-    ['devcontainer.json'] = { glyph = '', hl = 'MiniIconsAzure' },
-  },
-  filetype = {
-    dotenv = { glyph = '', hl = 'MiniIconsYellow' },
-  },
-})
+require('mini.icons').setup()
 
 package.preload['nvim-web-devicons'] = function()
   require('mini.icons').mock_nvim_web_devicons()
--- nvim/dot-config/nvim/lua/plugins/lspconfig.lua
+++ nvim/dot-config/nvim/lua/plugins/lspconfig.lua
@@ -1,11 +1,13 @@
 local gh = require('kevindurb.utils').gh
 vim.pack.add(vim.tbl_map(gh, { 'b0o/schemastore.nvim', 'neovim/nvim-lspconfig' }))
 
-local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
-for type, icon in pairs(signs) do
-  local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.fn.sign_define(
+  'DiagnosticSignError',
+  { text = ' ', texthl = 'DiagnosticSignError', numhl = 'DiagnosticSignError' }
+)
+vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn', numhl = 'DiagnosticSignWarn' })
+vim.fn.sign_define('DiagnosticSignHint', { text = ' ', texthl = 'DiagnosticSignHint', numhl = 'DiagnosticSignHint' })
+vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo', numhl = 'DiagnosticSignInfo' })
 
 vim.lsp.config('yamlls', {
   on_new_config = function(new_config)
