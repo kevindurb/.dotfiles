f050713e0d147551de76b14571bc830f6515c728
Kevin Durbin <kevindurb@fastmail.com>
2026-05-01 10:21:02 -0600

    modern replacements

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -11,6 +11,32 @@ if status is-interactive
   abbr --add k kubectl
   abbr --add ll ls -lah
 
+  if command -q eza
+    abbr --add ls eza
+    abbr --add ll 'eza -lah --git --group-directories-first'
+    abbr --add tree 'eza --tree'
+  end
+
+  if command -q bat
+      abbr --add cat bat
+  end
+
+  if command -q duf
+      abbr --add df duf
+  end
+
+  if command -q ncdu
+      abbr --add du ncdu
+  end
+
+  if command -q htop
+      abbr --add top htop
+  end
+
+  if command -q prettyping
+      abbr --add ping 'prettyping --nolegend'
+  end
+
   starship init fish | source
 
 end
