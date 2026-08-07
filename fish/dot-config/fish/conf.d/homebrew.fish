0dcaa683399338761c6133023926775d4b1dc4b6
Kevin Durbin <kevindurb@fastmail.com>
2026-08-07 13:05:36 -0600

    speed up homebrew

--- fish/dot-config/fish/conf.d/homebrew.fish
+++ fish/dot-config/fish/conf.d/homebrew.fish
@@ -4,10 +4,12 @@ set -gx HOMEBREW_CASK_OPTS         "--appdir=$HOME/Applications"
 set -gx HOMEBREW_BUNDLE_NO_UPGRADE 1
 set -gx HOMEBREW_NO_ENV_HINTS      1
 
-if test -d ~/.linuxbrew
-  eval (~/.linuxbrew/bin/brew shellenv fish | string collect)
-else if test -d /home/linuxbrew/.linuxbrew
-  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv fish | string collect)
-else if test -d /opt/homebrew
-  eval (/opt/homebrew/bin/brew shellenv fish | string collect)
+if not set -q HOMEBREW_PREFIX
+  if test -d ~/.linuxbrew
+    eval (~/.linuxbrew/bin/brew shellenv fish | string collect)
+  else if test -d /home/linuxbrew/.linuxbrew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv fish | string collect)
+  else if test -d /opt/homebrew
+    eval (/opt/homebrew/bin/brew shellenv fish | string collect)
+  end
 end
